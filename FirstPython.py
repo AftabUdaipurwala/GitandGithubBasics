702487b56196b8c6ad8e95f97e0f8e0963ee8a55
AftabUdaipurwala <udaipurwalaa@gmail.com>
2022-06-21 07:41:45 +0530

    Created new first python file while learning github

---
+++ FirstPython.py
@@ -0,0 +1,8 @@
+n1= int(input('Enter a Number :'))
+n2= int(input('Enter 2nd number to be added to 1st: '))
+def sum(n1,n2):
+    return n1+n2
+
+if __name__ == "__main__":
+    val = sum(n1,n2)
+    print(val)
