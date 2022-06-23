6257cd2a9a62d8b8389aa22e34fb043e4fb46470
AftabUdaipurwala <udaipurwalaa@gmail.com>
2022-06-23 09:19:50 +0530

    Learnt about diff tool and mergetool, i have used pycharms default diff instead of the meld tool
    Signed-off-by: AftabUdaipurwala <udaipurwalaa@gmail.com>

--- Hungry.py
+++ Hungry.py
@@ -3,6 +3,7 @@ if inp =='yes':
     print('Eat Samosa')
     print('Eat Pizza')
     print("Eat fries")
+    print('I have learnt merge changes now')
 elif inp =='no':
     inp = input('Are you thirsty')
     if inp =='yes':
